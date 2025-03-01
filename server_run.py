29ab61154601ca6ca6f44c5d45feaee196e32cce
Tyronne Salto <tyronne.salto@gmail.com>
2025-03-01 12:14:31 -0800

    generate state action/transition done

--- server_run.py
+++ server_run.py
@@ -1,11 +1,14 @@
 from utils.args_parser import ArgsParser
 from server.server import Server 
+from statemachine.statemachine import StateMachine
 
 
 if __name__ == '__main__':
-    parse_args = ArgsParser("Server", "./server/server.json")
-    args = parse_args.get_args()
+    # parse_args = ArgsParser("Server", "./server/server.json")
+    # args = parse_args.get_args()
 
-    server = Server(args.ip, args.port)
+    # server = Server(args.ip, args.port)
 
-    server.server_listen()
+    # server.server_listen()
+
+    sm = StateMachine(Server, "./server/server_state_actions.json", "./server/server_state_transition.json")
--- statemachine/statemachine.py
+++ statemachine/statemachine.py
@@ -1,3 +1,14 @@
+import enum
+import json
+
+class State(enum.Enum):
+    INIT = 0
+    LISTEN = 1
+    RECEIVE = 2
+    PROCESS = 3
+    SEND = 4
+    CLOSING = 5
+
 class StateMachine:
     def __init__(self, class_ref, state_action_file, state_transition_file):
         self.class_ref = class_ref
@@ -5,10 +16,35 @@ class StateMachine:
         self.state_transition = {}
 
         self.__generate_state_action(state_action_file)
+        self.__generate_state_transition(state_transition_file)
 
 
     def __generate_state_action(self, filepath):
-        pass
+        with open(filepath) as f:
+            state_action = json.load(f)
+        
+        try:
+            for key, value in state_action.items():
+                self.state_actions[State[key]] = getattr(self.class_ref, value)
+        except KeyError:
+            print(f"Warning: '{key}' is not a valid state in the State enum. Skipping...")
+        except AttributeError:
+            print(f"Warning: '{value}' is not a valid method in {self.class_ref.__name__}. Skipping...")
 
-    def __generate_state_transition(self, filepat):
+        #print(self.state_actions)
+
+    def __generate_state_transition(self, filepath):
+        with open(filepath) as f:
+            state_action = json.load(f)
+        try:
+            for key, value in state_action.items():
+                self.state_transition[State[key]] = []
+                for state in value:
+                    self.state_transition[State[key]].append(State[state]) 
+        except KeyError:
+            print(f"Warning: '{key}' is not a valid state in the State enum. Skipping...")
+        except AttributeError:
+            print(f"Warning: '{value}' is not a valid method in {self.class_ref.__name__}. Skipping...")
+
+        print(self.state_transition)
         pass

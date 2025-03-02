ee33978500eb0ebaffd40316990d6f296f2a4c47
Matthew <Matirix@live.com>
2025-03-01 16:54:28 -0800

    Added range for delay seconds

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -6,16 +6,17 @@ class Proxy:
     def __init__(self, proxy_ip: str, proxy_port: int):
         self.listen_ip= proxy_ip
         self.listen_port = proxy_port
-        self.target_ip = "127.0.0.1" # TODO Edit Later
-        self.target_port = 8081
         self.client_ip = None
         self.cient_port = None
+        # Edit later
+        self.target_ip = "127.0.0.1"
+        self.target_port = 8081
         self.client_drop = .1;
         self.server_drop = .1;
-        self.client_delay = .1;
-        self.server_delay = .1;
-        self.server_delay_time = 2;
-        self.client_delay_time = 2;
+        self.client_delay = .9;
+        self.server_delay = .9;
+        self.server_delay_time = "1-3";
+        self.client_delay_time = "1-3";
         self.proxy_socket = socket.socket()
 
     def proxy_init(self):
@@ -66,7 +67,7 @@ class Proxy:
             print("Client Packet to Server Fails")
             return
         if self.does_packet_delay(self.client_delay):
-            print(f"Client Packet is delayed by {self.client_delay_time} seconds")
+            print("Client ", end="")
             self.delay_by_seconds(self.client_delay_time);
         self.proxy_socket.sendto(message,(self.target_ip, self.target_port) )
 
@@ -82,7 +83,10 @@ class Proxy:
         """
         Delays packet send by seconds
         """
-        time.sleep(delay_time)
+        lower, upper = delay_time.split("-")
+        delay = random.randrange(int(lower), int(upper))
+        print(f"Packet is delayed by {delay} second(s)")
+        time.sleep(delay)
 
     def is_server(self, ip, port) -> bool:
         if ip == self.target_ip and port == self.target_port:
--- server_run.py
+++ server_run.py
@@ -1,9 +1,19 @@
 from utils.args_parser import ArgsParser
-from server.server import Server 
+from server.server import Server
 from statemachine.statemachine import StateMachine
 
 
 if __name__ == '__main__':
+    """
+    Start the server
+
+    So far Proxy server has hard-coded values for easy testing and simulating conditions.
+    Those are just the ip and port it binds to.
+
+    to run the server, use the following command:
+    python server_run.py --ip <ip_address> --port <port_number>
+    """
+
     parse_args = ArgsParser("Server", "./server/server.json")
     args = parse_args.get_args()
 
